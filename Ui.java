e869bd9509c745a8b367b226334fd41d16d0156b
mmittal2 <91178481+mmittal2@users.noreply.github.com>
2023-09-21 20:19:47 +0000

    Added code to print out instructions.

--- Ui.java
+++ Ui.java
@@ -1,5 +1,19 @@
 public class Ui {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Let's start calculating!\n");
+        printInstructions();
     }
+
+    public static void printInstructions() {
+        System.out.println("Instructions for how to use the calculator:");
+        System.out.println("\t1. The calculator only takes integers (no decimals).");
+        System.out.println("\t2. The calculator can perform the following functions: subtract, add, multiply, divide, and powers.");
+        System.out.println("  These are the symbols that represent each calculation:");
+        System.out.println("\t- subtract: -");
+        System.out.println("\t- add: +");
+        System.out.println("\t- multiply: *");
+        System.out.println("\t- divide: /");
+        System.out.println("\t- power: ^\n");
+    }
+
 }
