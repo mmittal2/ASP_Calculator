2531d11e5295cb077fa7fdb0a8a5891056103348
ChowdharyAarna <90006860+ChowdharyAarna@users.noreply.github.com>
2023-09-26 20:14:13 +0000

    Created getNumbers & parseAdd functions.

--- Translator.java
+++ Translator.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 public class Translator {
     public static void main(String[] args){
+        Engine e = new Engine();
+        Double result = calculate("+", 2.0, 3.0, e);
+        System.out.println(result);
+        System.out.println(parseAdd("2 * 34 + 7 / 3", e));
     }
 
     public String parseDivide(String calculation, Engine e) {
@@ -45,5 +49,55 @@ public class Translator {
         calculation = parseAdd(calculation, e);
         calculation = parseSubtract(calculation, e);
         return calculation;
+    }
+
+    public static ArrayList<Integer> getNumbers(String expression, char operator){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        // firstNum, lastNum, startingIndex, endingIndex
+        int location = expression.indexOf(operator);
+        int startingIndex = 0;
+        int endingIndex = 0;
+        String firstNum = "";
+        String lastNum = "";
+        for(int i = location - 2; i >= 0; i--){
+            if(expression.charAt(i) == ' '){
+                startingIndex = i;
+                break;
+            }
+            firstNum = expression.charAt(i) + firstNum;
+        }
+
+        for(int i = location + 2; i < expression.length(); i++){
+            if(expression.charAt(i) == ' '){
+                endingIndex = i;
+                break;
+            }
+            lastNum = lastNum + expression.charAt(i);
+        }
+
+        
+        numbers.add(Integer.valueOf(firstNum));
+        numbers.add(Integer.valueOf(lastNum));
+        numbers.add(startingIndex);
+        numbers.add(endingIndex);
+        
+        return numbers;
+    }
+
+    public static String parseAdd(String expression, Engine e){
+        
+        while(expression.indexOf('+') > -1){
+            ArrayList <Integer> numbers = getNumbers(expression, '+');
+            int firstNum = numbers.get(0);
+            int lastNum = numbers.get(1);
+            int startingIndex = numbers.get(2);
+            int endingIndex = numbers.get(3);
+
+
+            double answer = e.add((double) firstNum, (double) lastNum);
+
+            expression = expression.substring(0, startingIndex) + " " +  Integer.toString((int) answer) + expression.substring(endingIndex);
+        }
+        return expression;
     }
 }
